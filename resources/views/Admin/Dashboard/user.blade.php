889cea0495b238a05b20ee81b0ceb2c77329c142
msaimkz <msaimkzmsk2008@gmail.com>
2024-11-22 08:39:40 +0500

    User Page Start

--- resources/views/Admin/Dashboard/user.blade.php
+++ resources/views/Admin/Dashboard/user.blade.php
@@ -3,17 +3,29 @@
 @section('content')
 <div class="content">
 
-<div class="sale-dashboard-card-container">
-    <div class="sale-dashboard-card">
-        <div class="sale-dashboard-card-top">
-            <div class="sale-card-icon"></div>
-            
+    <div class="sale-main-heading">
+        <h2>User Statistics</h2>
+        <div class="sale-sub-heading">
+            <h4>
+                <a href="{{ route('Admin.dashboard.index') }}"> Dashboard</a>
+               
+            </h4>
+            <i class="fa-solid fa-angles-right"></i>
+            <h4>User Statistics</h4>
         </div>
-        <div class="sale-dashboard-card-bottom"></div>
-
     </div>
-</div>
-    
+
+    <div class="sale-dashboard-card-container">
+        <div class="sale-dashboard-card">
+            <div class="sale-dashboard-card-top">
+                <div class="sale-card-icon"></div>
+
+            </div>
+            <div class="sale-dashboard-card-bottom"></div>
+
+        </div>
+    </div>
+
 </div>
 @endsection
 @section('script')
