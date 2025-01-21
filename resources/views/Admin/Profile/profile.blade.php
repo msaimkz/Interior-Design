fbef09a4442fcdcb84751578a2fc22f8b62561e1
msaimkz <msaimkzmsk2008@gmail.com>
2025-01-21 14:11:12 +0500

    Profile Page Half Done

--- resources/views/Admin/Profile/profile.blade.php
+++ resources/views/Admin/Profile/profile.blade.php
@@ -15,6 +15,37 @@
         <div class="profile-background-image">
             <img src="{{ asset('Asset/Admin/img/Profile-Background.jpg') }}" alt="">
         </div>
+        <div class="profile-about">
+            <div class="profile-img">
+                <img src="{{ asset('Asset/Admin/img/men-avatar.jpg') }}" alt="">
+            </div>
+            <div class="profile-content">
+                <h3 class="profile-name">Muhammad Saim</h3>
+                <span class="profile-designation">Administration</span>
+                <div class="profile-location">
+                    <p>
+                        <i class="fa-solid fa-location-dot"></i>
+                       <span>Pakistan</span>
+                    </p>
+                    <p>
+                        <i class="fa-solid fa-city"></i>
+                        <span>Karachi, Sindh</span>
+                    </p>
+                </div>
+            </div>
+           
+            
+        </div>
+        <div class="profile-tab">
+            <div class="profile-tab-child">
+                <h4>About</h4>
+                <div class="line"></div>
+            </div>
+            <div class="profile-tab-child">
+                <h4>Edit Profile</h4>
+                <div class="line"></div>
+            </div>
+        </div>
     </div>
 </div>
 
