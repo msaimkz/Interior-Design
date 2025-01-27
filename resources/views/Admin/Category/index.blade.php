698af3aa69e11122ab313a095b0806aa4c3570c9
msaimkz <msaimkzmsk2008@gmail.com>
2025-01-27 17:20:17 +0500

    Category Page Half Done

--- resources/views/Admin/Category/index.blade.php
+++ resources/views/Admin/Category/index.blade.php
@@ -30,11 +30,116 @@
                         </button>
                     </div>
                 </div>
-                <div class="admin-main-table-body"></div>
-                <div class="admin-main-table-footer"></div>
-                
+                <div class="admin-main-table-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th class="CheckInputThead">
+                                    <input type="checkbox" class="form-check-input" name="CheckBoxSelection"
+                                        id="CheckBoxSelection">
+                                </th>
+                                <th>Sno:</th>
+                                <th>Name</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="CheckInputTbody">
+                                    <input type="checkbox" class="form-check-input" name="MultipleDeleteCheck"
+                                        id="MultipleDeleteCheck">
+                                </td>
+                                <td>
+                                    01
+                                </td>
+                                <td>
+                                    <div class="table-name">
+                                        <img src="{{ asset('Asset/Admin/img/table-img.jpg') }}" alt="">
+                                        <a href="#">Kitchen</a>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span class="table-badge table-success">Active</span>
+                                </td>
+                                <td>
+                                    <div class="table-action">
+                                        <a href="" class="table-action-btn" style="--i: 84 , 109, 254;">
+                                            <i class="fa-regular fa-eye"></i>
+                                        </a>
+                                        <a href="" class="table-action-btn" style="--i: 12 , 215, 177;">
+                                            <i class="ri-edit-line"></i>
+                                        </a>
+                                        <a href="" class="table-action-btn" style="--i: 254 ,84, 84;">
+                                            <i class="ri-delete-bin-line"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="CheckInputTbody">
+                                    <input type="checkbox" class="form-check-input" name="MultipleDeleteCheck"
+                                        id="MultipleDeleteCheck">
+                                </td>
+                                <td>
+                                    01
+                                </td>
+                                <td>
+                                    <div class="table-name">
+                                        <img src="{{ asset('Asset/Admin/img/table-img.jpg') }}" alt="">
+                                        <a href="#">Kitchen</a>
+                                    </div>
+                                </td>
+                                <td>
+                                    <span class="table-badge table-success">Active</span>
+                                </td>
+                                <td>
+                                    <div class="table-action">
+                                        <a href="" class="table-action-btn" style="--i: 84 , 109, 254;">
+                                            <i class="fa-regular fa-eye"></i>
+                                        </a>
+                                        <a href="" class="table-action-btn" style="--i: 12 , 215, 177;">
+                                            <i class="ri-edit-line"></i>
+                                        </a>
+                                        <a href="" class="table-action-btn" style="--i: 254 ,84, 84;">
+                                            <i class="ri-delete-bin-line"></i>
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="admin-main-table-footer">
+                    <p>
+                        Showing
+                        <b>1</b>
+                        to
+                        <b>6</b>
+                        of
+                        <b>10</b>
+                        entries
+                    </p>
+                    <div class="table-footer-pagination">
+                        <nav aria-label="...">
+                            <ul class="pagination">
+                              <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                              </li>
+                              <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                              <li class="page-item " aria-current="page">
+                                <a class="page-link" href="#">2</a>
+                              </li>
+                              <li class="page-item"><a class="page-link" href="#">3</a></li>
+                              <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                              </li>
+                            </ul>
+                          </nav>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
-    
 @endsection
